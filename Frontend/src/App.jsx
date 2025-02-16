fb63dba4e065aa57b085f128fc24c43de7aefba1
Niles <egbaidomehjamez@gmail.com>
2025-02-16 14:18:56 +0100

    Refactor frontend structure: replace main.jsx with index.jsx, add Navbar and context for workouts, implement Home page with workout fetching and form functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,44 +1,21 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages & components
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    try {
-      const res = axios.get("http://localhost:3000/api/workouts");
-      console.log(res);
-    } catch (error) {
-      console.error(error);
-    }
-  }, []);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
