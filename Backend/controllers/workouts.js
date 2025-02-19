8d8fb818eec32116908f20b79db777155da4b147
Niles <egbaidomehjamez@gmail.com>
2025-02-19 23:35:53 +0100

    Refactor workout controller: enhance input validation, improve error handling, and clean up commented code

--- Backend/controllers/workouts.js
+++ Backend/controllers/workouts.js
@@ -1,4 +1,5 @@
 const Workout = require("../models/workouts");
+const mongoose = require("mongoose");
 
 const { title, reps, load } = Workout;
 
@@ -17,25 +18,6 @@ exports.checkID = (req, res, next, val) => {
 exports.getWorkouts = async (req, res) => {
   res.setHeader("Content-Type", "application/json");
 
-  // const { title, load, reps } = req.body;
-
-  // let emptyFields = [];
-
-  // if (!title) {
-  //   emptyFields.push("title");
-  // }
-  // if (!load) {
-  //   emptyFields.push("load");
-  // }
-  // if (!reps) {
-  //   emptyFields.push("reps");
-  // }
-  // if (emptyFields.length > 0) {
-  //   return res
-  //     .status(400)
-  //     .json({ error: "Please fill in all fields", emptyFields });
-  // }
-
   try {
     const workouts = await Workout.find({}).sort({ createdAt: -1 });
     res.status(200).json(workouts);
@@ -45,21 +27,45 @@ exports.getWorkouts = async (req, res) => {
 };
 
 exports.postWorkout = async (req, res) => {
-  const { title, reps, load } = req.body;
+  const { title, load, reps } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!load) {
+    emptyFields.push("load");
+  }
+  if (!reps) {
+    emptyFields.push("reps");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all fields", emptyFields });
+  }
 
   try {
-    const workouts = await Workout.find({}).sort({ createdAt: -1 });
-
-    if (!workouts) {
-      return res.status(404).json({ message: "No workouts found" });
-    }
-
-    res.status(200).json(workouts);
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ message: "Error fetching workouts", error: error.message });
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
+
+  // try {
+  //   const workouts = await Workout.find({}).sort({ createdAt: -1 });
+
+  //   if (!workouts) {
+  //     return res.status(404).json({ message: "No workouts found" });
+  //   }
+
+  //   res.status(200).json(workouts);
+  // } catch (err) {
+  //   return res
+  //     .status(500)
+  //     .json({ message: "Error fetching workouts", error: error.message });
+  // }
   //   res.send({ mssg: "create workout" });
 };
 
@@ -75,9 +81,13 @@ exports.getWorkout = async (req, res) => {
 };
 
 exports.updateWorkout = async (req, res) => {
-  const id = req.params.id;
+  const { id } = req.params;
 
-  const workout = await mongoose.findOneAndUpdate(
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  const workout = await Workout.findOneAndUpdate(
     { _id: id },
     {
       ...req.body,
@@ -85,18 +95,24 @@ exports.updateWorkout = async (req, res) => {
   );
 
   if (!workout) {
-    return res.status(400).json({ error: "no workout found" });
+    return res.status(400).json({ error: "No such workout" });
   }
+
   res.status(200).json(workout);
 };
 
 exports.deleteWorkout = async (req, res) => {
-  const id = req.params.id;
+  const { id } = req.params;
 
-  const workout = await mongoose.findOneAndDelete({ _id: id });
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  const workout = await Workout.findOneAndDelete({ _id: id });
 
   if (!workout) {
-    return res.status(400).json({ error: "no workout found" });
+    return res.status(400).json({ error: "No such workout" });
   }
+
   res.status(200).json(workout);
 };
