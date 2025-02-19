89c5c5f4afcee8b45255c4fc36f9a757db4bf77d
Niles <egbaidomehjamez@gmail.com>
2025-02-19 23:47:13 +0100

    Implement user login functionality: add login method in user model and update login controller to handle authentication and token generation

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -9,7 +9,17 @@ const createToken = (_id) => {
 };
 
 exports.login = async (req, res) => {
-  res.send("Login");
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, user, token });
+  } catch (error) {
+    red.status(400).json({ error: error.message });
+  }
 };
 
 exports.signup = async (req, res) => {
@@ -23,6 +33,6 @@ exports.signup = async (req, res) => {
 
     res.status(200).json({ email, user, token });
   } catch (error) {
-    red.status(400).json({ error: error.message });
+    res.status(400).json({ error: error.message });
   }
 };
--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -45,4 +45,25 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("email and password are required");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("email does not exist");
+  }
+
+  const auth = await bcrypt.compare(password, user.password);
+
+  if (!auth) {
+    throw Error("password is incorrect");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
